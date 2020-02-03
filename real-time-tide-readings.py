98a475182bcd23ed77acae1fc2a31385c714536a
Akshay Doosa <akshay.doosa@arup.com>
2020-02-03 16:55:40 +1000

    Update real-time-tide-readings.py

--- real-time-tide-readings.py
+++ real-time-tide-readings.py
@@ -1,7 +1,9 @@
 import requests
-url = 'http://opendata.tmr.qld.gov.au/southport.txt'
+url = 'http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/AUS.csv'
 response = requests.get(url)
 if response.status_code != 200:
     print('Failed to get data:', response.status_code)
 else:
     print(response.text)
+# http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/tas/2019/2020/AUS.csv
+# http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/CAN.csv
