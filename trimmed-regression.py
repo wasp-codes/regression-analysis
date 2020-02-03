1b698e1d2fed6c54fef590a22cb40848e3137e07
Akshay Doosa <akshay.doosa@arup.com>
2020-02-03 12:05:48 +1000

    Updated trimmed-regression.py

---
+++ trimmed-regression.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
