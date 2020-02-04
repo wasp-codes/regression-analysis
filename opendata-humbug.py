4c6464fd40dcc70660ab536ee5265ae26e2e4f36
Akshay D <wasp@waspey-Air.local>
2020-02-05 01:14:04 +1000

    Changed df.loc range

--- opendata-humbug.py
+++ opendata-humbug.py
@@ -12,12 +12,12 @@ else:
     print(response.text[:28])
 
 df = pd.read_csv('http://opendata.tmr.qld.gov.au/Humbug_Wharf.txt', sep=" ", header=None, skiprows=5, engine='python')
-df = df.loc[11500:11520,[0,2]]
+df = df.loc[10500:11520,[0,2]]
 df = df.dropna()
 df.columns = ['Time/Date','Water Level in m LAT']
 df['Time/Date'] =  pd.to_datetime(df['Time/Date'], format='%d%m%Y%H%M')
 print(df)
 
 # graph
-df.plot(x = 'Time/Date', y = 'Water Level in m LAT', kind = 'scatter')
+df.plot(x = 'Time/Date', y = 'Water Level in m LAT', kind = 'line')
 plt.show()
