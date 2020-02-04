89c0d944cd1303f64acbeef20666ad987767b917
Akshay D <wasp@waspey-Air.local>
2020-02-05 01:02:19 +1000

    Changed Time/Date

---
+++ opendata-humbug.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+
+# import for graph
+import matplotlib.pyplot as plt
+
+url = 'http://opendata.tmr.qld.gov.au/Humbug_Wharf.txt'
+response = requests.get(url)
+if response.status_code != 200:
+    print('Failed to get data:', response.status_code)
+else:
+    print(response.text[:28])
+
+df = pd.read_csv('http://opendata.tmr.qld.gov.au/Humbug_Wharf.txt', sep=" ", header=None, skiprows=5, engine='python')
+df = df.loc[11500:11520,[0,2]]
+df = df.dropna()
+df.columns = ['Time/Date','Water Level in m LAT']
+df['Time/Date'] =  pd.to_datetime(df['Time/Date'], format='%d%m%Y%H%M')
+print(df)
+
+# graph
+df.plot(x = 'Time/Date', y = 'Water Level in m LAT', kind = 'scatter')
+plt.show()
