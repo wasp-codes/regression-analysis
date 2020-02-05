a17b5282d1547b62008fe6cbce8878f7419d6a32
Akshay <adusa27@gmail.com>
2020-02-05 14:36:03 +1000

    Replaced pd.datetime due to Futurewarning

--- opendata-half-tide.py
+++ opendata-half-tide.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 
+from datetime import datetime
+
 # Import for plots
 import matplotlib.pyplot as plt
 
@@ -14,15 +16,15 @@ else:
 
 readingsUrl = 'http://opendata.tmr.qld.gov.au/Half_Tide.txt'
 columnNames = ['Time/Date', 'Water Level in m LAT', 'N/A 1', 'N/A 2', 'N/A 3']
-dateparse = lambda x: pd.datetime.strptime(x, '%d%m%Y%H%M')
+#dateParse = lambda x: dt.datetime.strptime(x, '%d%m%Y%H%M')
 
-df = pd.read_csv(readingsUrl, sep="  ", names=columnNames, header=None, skiprows=5, parse_dates=['Time/Date'], date_parser=dateparse, engine='python')
-#df['Time/Date'] =  pd.to_datetime(df['Time/Date'], format='%d%m%Y%H%M')
+df = pd.read_csv(readingsUrl, sep="  ", names=columnNames, header=None, dtype= {'Time/Date': 'str'}, skiprows=5, engine='python')
+df['Time/Date'] = df['Time/Date'].apply(lambda x: datetime.strptime(x, '%d%m%Y%H%M'))
 
 print(df)
 
 # plots
-#df.loc[11500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Minute-level Readings', kind='line')
-#df.loc[10500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Hour-level Readings', kind='line')
-#df.loc[:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Day-level Readings', kind='line')
-#plt.show()
+df.loc[11500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Minute-level Readings', kind='line')
+df.loc[10500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Hour-level Readings', kind='line')
+df.loc[:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Day-level Readings', kind='line')
+plt.show()
