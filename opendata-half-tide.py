a56b7f5673dcd5f9013e7b298fb44ecdbbf47034
Akshay <adusa27@gmail.com>
2020-02-05 12:50:46 +1000

    Added lambda parser

--- opendata-half-tide.py
+++ opendata-half-tide.py
@@ -14,15 +14,15 @@ else:
 
 readingsUrl = 'http://opendata.tmr.qld.gov.au/Half_Tide.txt'
 columnNames = ['Time/Date', 'Water Level in m LAT', 'N/A 1', 'N/A 2', 'N/A 3']
-lambdaParser = lambda x: datetime.datetime.strptime(x, '%d%m%Y%H%M')
+dateparse = lambda x: pd.datetime.strptime(x, '%d%m%Y%H%M')
 
-df = pd.read_csv(readingsUrl, sep="  ", names=columnNames, header=None, skiprows=5, engine='python')
-df['Time/Date'] = pd.to_datetime(df['Time/Date'].str.strip(), format='%w%m%Y%H%M')
+df = pd.read_csv(readingsUrl, sep="  ", names=columnNames, header=None, skiprows=5, parse_dates=['Time/Date'], date_parser=dateparse, engine='python')
+#df['Time/Date'] =  pd.to_datetime(df['Time/Date'], format='%d%m%Y%H%M')
 
 print(df)
 
 # plots
-df.loc[11500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Minute-level Readings', kind='line')
-df.loc[10500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Hour-level Readings', kind='line')
-df.loc[:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Day-level Readings', kind='line')
-plt.show()
+#df.loc[11500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Minute-level Readings', kind='line')
+#df.loc[10500:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Hour-level Readings', kind='line')
+#df.loc[:11520].plot(x='Time/Date', y='Water Level in m LAT', title='Day-level Readings', kind='line')
+#plt.show()
